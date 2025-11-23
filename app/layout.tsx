207334fed1333f88e005055c969b1077957c9b90
Aliyu Iliyasu <aliyuiliyasu17@yahoo.com>
2025-11-23 03:41:40 +0100

    Login Works, WPA fix2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+//import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,7 +34,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
-        <Analytics />
+        {/* {<Analytics />}  */}
       </body>
     </html>
   );
